a7de80026523ed439a09122b3e313fc9be0e2825
김형익 <kiomnd2@naver.com>
2021-12-19 20:52:28 +0900

    first commit

---
+++ src/main/java/com/kiomnd2/cosmo/CosmoApplication.java
@@ -0,0 +1,13 @@
+package com.kiomnd2.cosmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiomnd2/cosmo/CosmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiomnd2.cosmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
